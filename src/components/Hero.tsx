7677bd7e052303fbfcff4e0062e0dd662b042b22
Martin <56572703+martingalmarino@users.noreply.github.com>
2025-09-16 19:04:58 -0300

    Update hero background image to use hero.svg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url('/images/hero-bg.jpg')`
+          backgroundImage: `url('/images/hero.svg')`
         }}
       />
       
